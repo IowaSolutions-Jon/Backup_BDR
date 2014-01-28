67486267beb83c5ff71c883d678f4d31a97da6c8
OpenShift guest <jon@iowasolutions.com>
2014-01-28 15:34:31 -0500

    Updating for ReturnFileDates.bat

---
+++ BDR_Monitoring/Scripts/ReturnFileDates.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM You will pass the name of the text file to the command and the target
+REM 
+REM FORMAT for Invocation :: ReturnFileDates.bat <filename> <target>
+REM 
+REM This will create a file on the local machine running the script that will retunr
+REM the remote information requested and put that into a filename that you specify.
+set output=%1.txt
+FOR %%? IN ("c:\users\jon.IASOLUTIONS\Desktop\PSTools\*") DO (
+	ECHO Filename:%%~n? >> %output%
+	ECHO File Extension:%%~x? >> %output%
+	ECHO File Attributes:%%~a? >> %output%
+	ECHO Located on Drive:%%~d? >> %output%
+	ECHO File Size:%%~z? >> %output%
+	ECHO Last-Modified Date:%%~t? >> %output%
+	ECHO. >> %output%
+)
